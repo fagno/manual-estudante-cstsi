17c512bcafb807f2fd5dbeea90724be0b25902f4
Fagno <fagno.fonseca@gmail.com>
2021-05-31 16:46:44 -0300

    Update nome da branch no arquivo gh-pages.sh.

--- gh-pages.sh
+++ gh-pages.sh
@@ -18,10 +18,10 @@ fi
 if [[ $1 == "--no-history" || $1 == "-nh" ]]; then
     git branch -D gh-pages 1>/dev/null 2>/dev/null
     echo "The history for the gh-pages branch won't be kept."
-    git checkout --orphan gh-pages master 1>/dev/null || (echo "It was not possible to change to gh-pages branch" && exit -1)
+    git checkout --orphan gh-pages main 1>/dev/null || (echo "It was not possible to change to gh-pages branch" && exit -1)
 else
     echo "Change history in the gh-pages branch will be kept."
-    git checkout master -B gh-pages 1>/dev/null || (echo "It was not possible to change to gh-pages branch" && exit -1)
+    git checkout main -B gh-pages 1>/dev/null || (echo "It was not possible to change to gh-pages branch" && exit -1)
 fi
 echo ""
 
@@ -55,4 +55,4 @@ echo ""
 echo "Pushing gh-pages branch to GitHub"
 git push -f --quiet --set-upstream origin gh-pages 1>/dev/null 
 
-git checkout master 1>/dev/null 
+git checkout main 1>/dev/null 
